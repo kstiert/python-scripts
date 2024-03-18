33696a77f3845eaf3146f8885a2837f13528249d
Kira <devin.stiert@gmail.com>
2024-03-18 08:35:29 -0400

    Fix build

--- ynab_homeassistant/ynab/model/categories_response.py
+++ ynab_homeassistant/ynab/model/categories_response.py
@@ -2,9 +2,11 @@ from typing import List
 
 from ynab_homeassistant.ynab.model.category_group import CategoryGroup
 
+
 class CategoriesResponse:
     category_groups: List[CategoryGroup]
     server_knowledge: int
+
 
 class CategoriesResponseWrapper:
     data: CategoriesResponse
